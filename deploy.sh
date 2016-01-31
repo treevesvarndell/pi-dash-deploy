69631c7c2b7a8efca1bea12f9c1e704e669bb90a
Trevor Mac <treevesvarn@gmail.com>
2016-01-31 22:14:52 +0000

    Use ansible

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-pgrep -f manage.py runserver | xargs kill
-
-rm -rf ~/deploy
-mkdir ~/deploy
-
-cd ~/deploy
-
-git clone https://github.com/treevesvarndell/dashboard.git
-
-cd ~/deploy/dashboard
-
-virtualenv venv
-source venv/bin/activate
-
-pip install -r requirements.txt
-
-python manage.py bower install
-python manage.py runserver 0.0.0.0:8080
-
+ansible-playbook playbooks/install.yaml -i hosts.yaml
